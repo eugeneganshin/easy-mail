74bc42f08d17c1bad0099597495a82d2649322cd
Eugene <e.s.ganshin.study@gmail.com>
2020-08-03 21:24:16 +0300

    CLIENT: socket io

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -10,7 +10,7 @@ import socketReducer from './middleware/socket';
 import modalReducer from './reducers/modals';
 import logger from './middleware/logger';
 
-let socket = io('http://localhost:5000');
+let socket = io.connect();
 
 let socketIoMiddleware = createSocketIoMiddleware(socket, 'server/');
 
